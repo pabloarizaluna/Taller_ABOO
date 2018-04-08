2321e990326a0a65269c18f5778974783b8c78c3
Pablo Ariza Luna <pablo_ariza@javeriana.edu.co>
2018-04-08 14:36:20 -0500

    Arreglo de la funcion Build_OptBinTree

--- Build_OptBinTree.cxx
+++ Build_OptBinTree.cxx
@@ -6,33 +6,32 @@ std::vector<std::vector<int>> Build_OptBinTree(std::vector<float> P, std::vector
   std::vector<float> aux_float(P.size(), 0.0);
 
   std::vector<std::vector<int>> R(P.size(), aux_int);
-  std::vector<std::vector<float>> W(P.size(), aux_float);
+  std::vector<std::vector<float>> W(P.size() + 1, aux_float);
   std::vector<std::vector<float>> M(P.size() + 1, aux_float);
 
-  for(int i = 1; i < P.size(); i++)
+  for(int i = 1; i < P.size() + 1; i++)
   {
-    W[i][i] = Q[i - 1] + P[i] + Q[i];
-    M[i][i] = Q[i - 1];
-
-    for(int j = i + 1; j < P.size(); i++)
+    W[i][i - 1] = Q[i - 1];
+    M[i][i - 1] = Q[i - 1];
+    for(int j = i; j < P.size(); j++)
     {
       W[i][j] = W[i][j - 1] + P[j] + Q[j];
       M[i][j] = std::numeric_limits<float>::infinity();
     }
   }
 
-  for(int l = 1; l < P.size(); l++)
+  for(int l = 1; l < P.size() + 1; l++)
   {
-    for(int i = 1; P.size() - l + 1; i++)
+    for(int i = 1; i < P.size() - l + 2; i++)
     {
       int j = i + l - 1;
       for(int r = i; r < j; r++)
       {
-        float v = M[i][r - 1] + M[r + 1][j] + W[i][j];
-        if(v < M[i][j])
+        float v = M[i][r - 1] + M[r + 1][j - 1] + W[i][j - 1];
+        if(v < M[i][j - 1])
         {
-          M[i][j] = v;
-          R[i][j] = r;
+          M[i][j - 1] = v;
+          R[i][j - 1] = r;
         }
       }
     }
